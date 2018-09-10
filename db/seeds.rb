db99355c58bca4dda38555402a00d3aa64374316
Adam <ahanson845@gmail.com>
2018-09-10 18:22:57 -0500

    updated master

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Speaker.create({first_name: "Dani", last_name: "Zaghian", email: "danielle@actualize.co"})
+Speaker.create({first_name: "Josh", last_name: "Nixon", email: "josh@actualize.co"})
+Speaker.create({first_name: "Peter", last_name: "Jang", email: "peter@actualize.co"})
+Speaker.create({first_name: "Brian", last_name: "Rice", email: "brian@actualize.co"})
+
+Meeting.create({title: "Apple", agenda: "New Iphone rollout", time: "4 PM", location: "Chicago", remote: "true"})
+Meeting.create({title: "HR Follow up", agenda: "fire 50 employees", time: "3 PM", location: "River North, Chicago", remote: "false"})
+Meeting.create({title: "Charmin Pitch", agenda: "Marketing", time: "9 AM", location: "Glenview, IL", remote: "false"})
